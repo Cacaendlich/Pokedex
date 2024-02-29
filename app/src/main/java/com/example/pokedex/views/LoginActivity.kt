daa93959d7a1de05627b86dc31138451a1190cd6
Caca Endlich <camilaendlichpimenta.trab@gmail.com>
2024-02-29 10:48:54 -0300

    shared e msg
    tirei o shared prefs da fun loginIsValid(), e levei a msgs de erro pra view.

--- app/src/main/java/com/example/pokedex/views/LoginActivity.kt
+++ app/src/main/java/com/example/pokedex/views/LoginActivity.kt
@@ -1,7 +1,9 @@
 package com.example.pokedex.views
 
+import android.content.Context
 import android.os.Bundle
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.example.pokedex.databinding.ActivityFormLoginBinding
@@ -23,22 +25,38 @@ class LoginActivity : AppCompatActivity() {
 
         viewModel = ViewModelProvider(this)[LoginViewModel::class.java]
 
-        if(viewModel.sharedPrefsIsNotEmpty(this)){
-            val emailConsult = viewModel.getStoredLoginData(this)
-            emailEditText.setText(emailConsult)
+        var emailNoShared=  false
+        viewModel.sharedPrefsIsNotEmpty(this)
+
+        viewModel.loginSharedPrefsState.observe(this) { state ->
+            when (state) {
+                LoginViewModel.LoginSharedPrefsState.HAS_LOGIN_DATA -> {
+                    val emailSave = viewModel.getStoredLoginData(this)
+                    emailEditText.setText((emailSave))
+                }
+                LoginViewModel.LoginSharedPrefsState.NO_LOGIN_DATA -> emailNoShared = true
+                null -> showMessage(this,"ERROR")
+            }
         }
 
         binding.buttonLogin.setOnClickListener {
             viewModel.loginIsValid(email = emailEditText.text.toString(), password = passwordEditText.text.toString(), context = this)
+
+            if(emailNoShared) {
+                viewModel.saveLoginData(this, emailEditText)
+            }
         }
 
         viewModel.loginState.observe(this) { state ->
             when (state) {
-                LoginViewModel.LoginState.SUCCESS -> viewModel.showMessage(this,"LOGIN SUCCESS!")
-                LoginViewModel.LoginState.INVALID_CREDENTIALS -> viewModel.showMessage(this,"Invalid credentials.")
-                LoginViewModel.LoginState.EMPTY_FIELDS -> viewModel.showMessage(this,"Please enter an email and password.")
-                null -> viewModel.showMessage(this,"ERROR")
+                LoginViewModel.LoginState.SUCCESS -> showMessage(this,"LOGIN SUCCESS!")
+                LoginViewModel.LoginState.INVALID_CREDENTIALS -> showMessage(this,"Invalid credentials.")
+                LoginViewModel.LoginState.EMPTY_FIELDS -> showMessage(this,"Please enter an email and password.")
+                null -> showMessage(this,"ERROR")
             }
         }
+    }
+    private fun showMessage(context: Context, message: String) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
     }
 }
