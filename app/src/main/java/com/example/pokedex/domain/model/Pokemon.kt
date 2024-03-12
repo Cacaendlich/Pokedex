69da82b79d9e5f43e6763a5faa87bd4bb279125a
Caca Endlich <camilaendlichpimenta.trab@gmail.com>
2024-03-12 15:12:29 -0300

    Update Pokemon.kt

--- app/src/main/java/com/example/pokedex/domain/model/Pokemon.kt
+++ app/src/main/java/com/example/pokedex/domain/model/Pokemon.kt
@@ -1,7 +1,7 @@
 package com.example.pokedex.domain.model
 
 data class Pokemon(
-    val id: String,
+    val id: Int,
     val img: String,
     val name: String,
     val type: List<String>,
@@ -10,4 +10,40 @@ data class Pokemon(
     val def: Int,
     val spd: Int,
     val xp: Int
-)
+) {
+    fun getId(): Int {
+        return id
+    }
+
+    fun getImg(): String {
+        return img
+    }
+
+    fun getName(): String {
+        return name
+    }
+
+    fun getType(): List<String> {
+        return type
+    }
+
+    fun getHp(): Int {
+        return hp
+    }
+
+    fun getAtk(): Int {
+        return atk
+    }
+    fun getDef(): Int {
+        return def
+    }
+
+    fun getSpd(): Int {
+        return spd
+    }
+
+    fun getXp(): Int {
+        return xp
+    }
+
+}
