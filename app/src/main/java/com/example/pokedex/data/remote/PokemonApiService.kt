4ba76cec71880cdae98c9f3850f9d890de9b8d73
Caca Endlich <camilaendlichpimenta.trab@gmail.com>
2024-03-13 09:51:50 -0300

    Update PokemonApiService.kt

--- app/src/main/java/com/example/pokedex/data/remote/PokemonApiService.kt
+++ app/src/main/java/com/example/pokedex/data/remote/PokemonApiService.kt
@@ -4,6 +4,6 @@ import com.example.pokedex.domain.model.PokemonResponse
 import retrofit2.http.GET
 
 interface PokemonApiService {
-    @GET("pokemon/random")
+    @GET("pokemon_item.xml/random")
     suspend fun getRandomPokemon(): PokemonResponse
 }
