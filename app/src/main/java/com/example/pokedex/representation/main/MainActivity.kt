dbc8bef18a584e9db92229c9d76e3176c90c2c6a
Caca Endlich <camilaendlichpimenta.trab@gmail.com>
2024-03-13 15:37:52 -0300

    Update MainActivity.kt

--- app/src/main/java/com/example/pokedex/representation/main/MainActivity.kt
+++ app/src/main/java/com/example/pokedex/representation/main/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.example.pokedex.representation.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.example.pokedex.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+
     }
 }
