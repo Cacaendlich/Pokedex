c8f5e8202e606c5d4600fbb0e614ddd4d7fd1b88
Caca Endlich <camilaendlichpimenta.trab@gmail.com>
2024-03-15 09:58:05 -0300

    Update RetrofitClient.kt

--- app/src/main/java/com/example/pokedex/data/network/RetrofitClient.kt
+++ app/src/main/java/com/example/pokedex/data/network/RetrofitClient.kt
@@ -1,17 +1,25 @@
 package com.example.pokedex.data.network
 
+import com.example.pokedex.data.remote.PokemonApiService
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
 
 object RetrofitClient {
+    //https://pokeapi.co/api/v2/pokemon/?limit=100
+    //https://pokeapi.co/api/v2/pokemon/
     private const val BASE_URL = "https://pokeapi.co/api/v2/"
+    private val service: PokemonApiService
 
     private val okHttpClient = OkHttpClient.Builder().build()
 
-    val retrofit: Retrofit = Retrofit.Builder()
-        .baseUrl(BASE_URL)
-        .client(okHttpClient)
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
+    init {
+        val retrofit: Retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        service = retrofit.create(PokemonApiService::class.java)
+    }
 }
