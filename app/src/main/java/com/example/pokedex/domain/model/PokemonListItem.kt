42cc556ec06a282e90efaeecc61b38116fa637d9
Caca Endlich <camilaendlichpimenta.trab@gmail.com>
2024-03-18 14:55:38 -0300

    Delete PokemonListItem.kt

--- app/src/main/java/com/example/pokedex/domain/model/PokemonListItem.kt
+++
@@ -1,8 +0,0 @@
-package com.example.pokedex.domain.model
-
-import com.google.gson.annotations.SerializedName
-
-data class PokemonListItem(
-    @SerializedName("name") val name: String,
-    @SerializedName("url") val url: String
-)
