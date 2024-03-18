fedd15228090007a307c5c738083c1b6167c1756
Caca Endlich <camilaendlichpimenta.trab@gmail.com>
2024-03-18 14:55:33 -0300

    Update PokemonApiResult.kt

--- app/src/main/java/com/example/pokedex/domain/model/PokemonApiResult.kt
+++ app/src/main/java/com/example/pokedex/domain/model/PokemonApiResult.kt
@@ -1,17 +1,21 @@
 package com.example.pokedex.domain.model
 
+// Representa o resultado da lista de Pokémons obtidos da API
 data class PokemonsApiResult(
     val count: Int,
     val next: String?,
     val previous: String?,
-    val results: List<Pokemon>
+    val results: List<PokemonResult>
 )
 
+// Representa um resultado individual de Pokémon na lista.
 data class PokemonResult(
     val name: String,
-    val url: String
+    val url: String,
 )
 
+// Representa os dados necessários de um Pokémon, incluindo a URL da imagem.
 data class PokemonApiResult(
+    val imageUrl: String,
     val name: String,
 )
