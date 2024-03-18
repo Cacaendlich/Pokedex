e71272938ec5f34514bcf47e6c8b5109a67e2c16
Caca Endlich <camilaendlichpimenta.trab@gmail.com>
2024-03-18 14:55:28 -0300

    Update PokemonApiService.kt

--- app/src/main/java/com/example/pokedex/data/remote/PokemonApiService.kt
+++ app/src/main/java/com/example/pokedex/data/remote/PokemonApiService.kt
@@ -1,6 +1,7 @@
 package com.example.pokedex.data.remote
 
 
+import com.example.pokedex.domain.model.PokemonApiResult
 import com.example.pokedex.domain.model.PokemonsApiResult
 import retrofit2.Call
 import retrofit2.http.GET
@@ -8,7 +9,7 @@ import retrofit2.http.Query
 
 interface PokemonApiService {
     @GET("pokemon")
-    fun listPokemons(@Query("limit") limit: Int = 100): Call<PokemonsApiResult>
-    @GET("pokemon/{id}")
-    fun getPokemon(id: Int): Call<PokemonsApiResult>
+    fun listPokemons(@Query("limit") limit: Int): Call<PokemonsApiResult>
+    @GET("pokemon/{number}")
+    fun getPokemon(number: Int): Call<PokemonApiResult>
 }
