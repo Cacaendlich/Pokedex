c07d499964cdcf81901fbd46335df3174edd035f
Caca Endlich <camilaendlichpimenta.trab@gmail.com>
2024-03-18 14:55:14 -0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,19 +52,29 @@ android {
 }
 
 dependencies {
+    // Kotlin coroutines para facilitar a programação assíncrona
     implementation("androidx.core:core-ktx:1.12.0")
+
+    // Biblioteca do Android Jetpack para suporte a ciclo de vida
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+
+    // Biblioteca do Android Jetpack para integração com Jetpack Compose
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+
+    // Bibliotecas de suporte do Android para compatibilidade com versões antigas
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    // Anotações de suporte do Android
     implementation("com.android.support:support-annotations:28.0.0")
-    implementation("androidx.core:core-ktx:1.12.0")
+
+    // Testes unitários e de instrumentação
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -72,7 +82,11 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    // Extensões do Android Jetpack para o ciclo de vida
     implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
+
+    // RecyclerView para exibição de listas
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("androidx.recyclerview:recyclerview-selection:1.1.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
@@ -80,13 +94,14 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+
+    // Retrofit para comunicação de rede
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:okhttp:4.9.1")
     implementation("com.squareup.okhttp3:logging-interceptor:4.9.1")
+
+    // Glide para carregamento de imagens
     implementation("com.github.bumptech.glide:glide:4.12.0")
     annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
-    implementation("me.sargunvohra.lib:pokekotlin:2.5.0")
-    implementation("com.github.bumptech.glide: glide: 4.11.0")
-    implementation("com.github.bumptech.glide: compiler: 4.11.0")
 }
