44eb22066152ab14ca3166b9d1e6dce65204a3ed
Caca Endlich <camilaendlichpimenta.trab@gmail.com>
2024-03-18 14:55:24 -0300

    Update RetrofitClient.kt

--- app/src/main/java/com/example/pokedex/data/network/RetrofitClient.kt
+++ app/src/main/java/com/example/pokedex/data/network/RetrofitClient.kt
@@ -1,7 +1,13 @@
 package com.example.pokedex.data.network
 
+import android.util.Log
 import com.example.pokedex.data.remote.PokemonApiService
+import com.example.pokedex.domain.model.PokemonApiResult
+import com.example.pokedex.domain.model.PokemonsApiResult
 import okhttp3.OkHttpClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.create
@@ -21,5 +27,17 @@ object RetrofitClient {
             .addConverterFactory(GsonConverterFactory.create())
             .build()
         service = retrofit.create(PokemonApiService::class.java)
+    }
+
+    fun listPokemons(limit: Int = 100): PokemonsApiResult? {
+        val call = service.listPokemons(limit)
+
+        return call.execute().body()
+    }
+
+    fun getPokemon(id: Int): PokemonApiResult? {
+        val call = service.getPokemon(id)
+
+        return call.execute().body()
     }
 }
