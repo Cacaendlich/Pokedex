89b92194de0c3046d2993356e47d39f1a903bac1
Caca Endlich <camilaendlichpimenta.trab@gmail.com>
2024-03-19 14:46:22 -0300

    Update MainViewModel.kt

--- app/src/main/java/com/example/pokedex/presentation/main/MainViewModel.kt
+++ app/src/main/java/com/example/pokedex/presentation/main/MainViewModel.kt
@@ -1,33 +1,44 @@
 package com.example.pokedex.presentation.main
 
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.pokedex.data.network.RetrofitClient
+import com.example.pokedex.domain.model.Pokemon
 
 class MainViewModel: ViewModel() {
-//    private var pokemons = MutableLiveData<List<Pokemon?>>()
-//
-//    init {
-//        Thread(Runnable {
-//            loadPokemons()
-//        }).start()
-//    }
-    private fun loadPokemons() {
-//        val pokemonsApiResult = RetrofitClient.listPokemons()
-//
-//        pokemonsApiResult?.results?.let {
-//            pokemons.postValue(it.map { pokemonResult ->
-//                val id = pokemonResult.url
-//                    .replace("https://pokeapi.co/api/v2/pokemon/", "")
-//                    .replace("/", "").toInt()
-//
-//                val pokemonApiResult = RetrofitClient.getPokemon(id)
-//
-//                pokemonApiResult?.let {
-//                    Pokemon(
-//                        pokemonApiResult.id,
-//                        pokemonApiResult.name
-//                    )
-//                }
-//            })
-//        }
+    var pokemons = MutableLiveData<List<Pokemon?>>()
+
+    init {
+        Thread(Runnable {
+            loadPokemons()
+        }).start()
     }
+
+    private fun loadPokemons() {
+        val pokemonsApiResult = RetrofitClient.listPokemons()
+
+        pokemonsApiResult?.results?.let {
+
+
+            pokemons.postValue(it.map { pokemonResult ->
+                val number = pokemonResult.url
+                    .removePrefix("https://pokeapi.co/api/v2/pokemon/")
+                    .removeSuffix("/").toInt()
+
+                val pokemonApiResult = RetrofitClient.getPokemon(number)
+
+                pokemonApiResult?.let {
+                    Pokemon(
+                        pokemonApiResult.id,
+                        pokemonApiResult.name
+                    )
+                }
+            })
+
+        }
+
+    }
+
+
 }
