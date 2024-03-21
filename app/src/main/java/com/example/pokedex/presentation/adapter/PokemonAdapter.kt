389e9372a41eeb6cd1d1fb978c2bcb501f52d19d
Caca Endlich <camilaendlichpimenta.trab@gmail.com>
2024-03-21 12:40:17 -0300

    Update PokemonAdapter.kt

--- app/src/main/java/com/example/pokedex/presentation/adapter/PokemonAdapter.kt
+++ app/src/main/java/com/example/pokedex/presentation/adapter/PokemonAdapter.kt
@@ -1,13 +1,11 @@
 package com.example.pokedex.presentation.adapter
 
 import android.graphics.Bitmap
-import android.graphics.drawable.BitmapDrawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.annotation.ColorInt
 import androidx.cardview.widget.CardView
 import androidx.palette.graphics.Palette
 import androidx.recyclerview.widget.RecyclerView
@@ -19,7 +17,7 @@ import com.example.pokedex.domain.model.Pokemon
 
 class PokemonAdapter(
     private val mPokemonList: List<Pokemon?>
-): RecyclerView.Adapter<PokemonAdapter.PokemonViewHolder>() {
+) : RecyclerView.Adapter<PokemonAdapter.PokemonViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PokemonViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.pokemon_item, parent, false)
@@ -33,60 +31,73 @@ class PokemonAdapter(
         holder.bindView(currentItem)
     }
 
-    inner class PokemonViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+    inner class PokemonViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         //referencias de dados
         private val mImageViewPokemon: ImageView = itemView.findViewById(R.id.image_view_pokemon)
         private val mNameViewPokemon: TextView = itemView.findViewById(R.id.name_view_pokemon)
         private val mCardViewPokemon: CardView = itemView.findViewById(R.id.card_view_pokemon)
-        private val sampleSize = 100
 
         fun bindView(currentItem: Pokemon?) {
-
-            Glide.with(itemView.context)
-                .asBitmap()
-                .load(currentItem?.imageUrl)
-                .into(object : CustomTarget<Bitmap>() {
-                    override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
-                        mImageViewPokemon.setImageBitmap(resource)
-                        extractMainColor(resource) { color ->
-                            mCardViewPokemon.setCardBackgroundColor(color)
-
-
-                        }
-                    }
-
-
-                    override fun onLoadCleared(placeholder: android.graphics.drawable.Drawable?) {
-                        // Placeholder cleanup
-                    }
-                })
-
-            if (currentItem != null) {
+            currentItem?.let { pokemon ->
+                loadPokemonImage(pokemon.imageUrl)
                 mNameViewPokemon.text = currentItem.name
             }
-
         }
 
-        // Extrai a cor principal do Pokémon da imagem usando a biblioteca Palette
-        private fun extractMainColor(bitmap: Bitmap, onColorExtracted: (Int) -> Unit) {
-            // Determinar as dimensões do retângulo de recorte (pode precisar ser ajustado dependendo da imagem)
-            val cropLeft = bitmap.width / 4
-            val cropTop = bitmap.height / 4
-            val cropRight = bitmap.width * 3 / 4
-            val cropBottom = bitmap.height * 3 / 4
-
-            // Recortar a imagem
-            val croppedBitmap = Bitmap.createBitmap(bitmap, cropLeft, cropTop, cropRight - cropLeft, cropBottom - cropTop)
-
-            // Passar o bitmap recortado para a Palette para extrair a cor predominante
-            Palette.from(croppedBitmap).generate { palette ->
-                val dominantColor = palette?.dominantSwatch?.rgb
-                dominantColor?.let {
-                    onColorExtracted(it)
-                }
+        private fun loadPokemonImage(imageUrl: String) {
+            imageUrl.let { url ->
+                Glide.with(itemView.context)
+                    .asBitmap()
+                    .load(url)
+                    .into(PokemonImageTarget())
             }
         }
 
+        private inner class PokemonImageTarget : CustomTarget<Bitmap>() {
+            override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                mImageViewPokemon.setImageBitmap(resource)
+                extractMainColor(resource) { color ->
+                    mCardViewPokemon.setCardBackgroundColor(color)
+                }
+            }
 
+            override fun onLoadCleared(placeholder: android.graphics.drawable.Drawable?) {
+                // TODO: Implementar a lógica para limpar recursos
+                // quando a carga da imagem é cancelada ou quando
+                // a imagem não está mais disponível.
+            }
+        }
     }
+
+    // Extrai a cor principal do Pokémon da imagem usando a biblioteca Palette
+    private fun extractMainColor(bitmap: Bitmap, onColorExtracted: (Int) -> Unit) {
+        val croppedBitmap = cropBitmap(bitmap)
+
+        // Passar o bitmap recortado para a Palette para extrair a cor predominante
+        Palette.from(croppedBitmap).generate { palette ->
+            val dominantColor = palette?.dominantSwatch?.rgb
+            dominantColor?.let {
+                onColorExtracted(it)
+            }
+        }
     }
+
+    private fun cropBitmap(bitmap: Bitmap): Bitmap {
+        // Determinar as dimensões do retângulo de recorte (pode precisar ser ajustado dependendo da imagem)
+        val cropLeft = bitmap.width / 4
+        val cropTop = bitmap.height / 4
+        val cropRight = bitmap.width * 3 / 4
+        val cropBottom = bitmap.height * 3 / 4
+
+        // Recortar a imagem
+        return Bitmap.createBitmap(
+            bitmap,
+            cropLeft,
+            cropTop,
+            cropRight - cropLeft,
+            cropBottom - cropTop
+        )
+    }
+
+
+}
