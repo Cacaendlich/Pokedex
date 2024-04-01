469c4a5f32211a29dffe1e93d238209c78792a93
Caca Endlich <camilaendlichpimenta.trab@gmail.com>
2024-04-01 15:36:09 -0300

    Update MainViewModel.kt

--- app/src/main/java/com/example/pokedex/presentation/main/MainViewModel.kt
+++ app/src/main/java/com/example/pokedex/presentation/main/MainViewModel.kt
@@ -7,6 +7,8 @@ import com.example.pokedex.domain.model.Pokemon
 
 class MainViewModel: ViewModel() {
     var pokemonsState = MutableLiveData<List<Pokemon?>>()
+    var isLoading = false
+
 
     init {
         Thread {
@@ -36,6 +38,28 @@ class MainViewModel: ViewModel() {
         }
 
     }
+
+    fun loadMorePokemons() {
+        if (!isLoading) {
+            isLoading = true
+            val currentOffset = pokemonsState.value?.size ?: 0
+            Thread {
+                val pokemonsApiResultAPI = RetrofitClient.listPokemons(limit = 10, offset = currentOffset)
+                pokemonsApiResultAPI?.results?.let { newPokemons ->
+                    val existingPokemons = pokemonsState.value?.toMutableList() ?: mutableListOf()
+                    existingPokemons.addAll(newPokemons.mapNotNull { pokemonResult ->
+                        val name = pokemonResult.name
+                        val pokemonApiResult = RetrofitClient.getPokemon(name)
+                        pokemonApiResult?.let { Pokemon(it.id, it.name) }
+                    })
+                    pokemonsState.postValue(existingPokemons)
+                }
+                isLoading = false
+            }.start()
+        }
+    }
+
+
 
 
 }
