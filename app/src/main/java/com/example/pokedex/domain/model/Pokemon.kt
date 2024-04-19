2971499237c1c04af2480f0a7fec5b703e0b82f3
Caca Endlich <camilaendlichpimenta.trab@gmail.com>
2024-04-19 15:46:59 -0300

    Update Pokemon.kt

--- app/src/main/java/com/example/pokedex/domain/model/Pokemon.kt
+++ app/src/main/java/com/example/pokedex/domain/model/Pokemon.kt
@@ -4,6 +4,7 @@ package com.example.pokedex.domain.model
 data class Pokemon(
     val number: Int,
     val name: String,
+    var favorite: Boolean = false
 ) {
     val imageUrl by lazy { "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${number}.png" }
 
