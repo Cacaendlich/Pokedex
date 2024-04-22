4ec9f248fdc9ae305dfd58d40d3c703f1236a3c9
Caca Endlich <camilaendlichpimenta.trab@gmail.com>
2024-04-22 12:19:50 -0300

    Create PokemonDao.kt

---
+++ app/src/main/java/com/example/pokedex/data/local/dao/PokemonDao.kt
@@ -0,0 +1,20 @@
+package com.example.pokedex.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.pokedex.data.local.model.PokemonEntity
+import com.example.pokedex.domain.model.Pokemon
+
+@Dao
+interface PokemonDao {
+    @Insert
+    suspend fun insertPokemon(pokemon: PokemonEntity)
+
+    @Delete
+    suspend fun deletePokemon(pokemon: PokemonEntity)
+
+    @Query("SELECT * FROM Pokemons_favorites")
+    suspend fun getAllPokemons(): List<PokemonEntity>
+}
