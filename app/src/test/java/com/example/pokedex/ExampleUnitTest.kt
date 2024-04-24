379dc7e968e4ba23071300b560bab926efcce3e8
Caca Endlich <camilaendlichpimenta.trab@gmail.com>
2024-04-24 12:25:28 -0300

    test deletePokemon

--- app/src/test/java/com/example/pokedex/ExampleUnitTest.kt
+++ app/src/test/java/com/example/pokedex/ExampleUnitTest.kt
@@ -16,31 +16,46 @@ import org.mockito.MockitoAnnotations
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class PokemonDaoUnitTest {
-    @Mock ////@Mock Esta é uma anotação que é usada para criar um mock de uma classe ou interface
+    @Mock // Referência simulada para PokemonDao
     lateinit var pokemonDao: PokemonDao
 
-    @Before //@Before Esta é uma anotação que é usada em métodos de teste para indicar que eles devem ser executados antes de cada método de teste na classe.
+    @Before // Configuração pré-teste
     fun setUp() {
         MockitoAnnotations.openMocks(this)
     }
 
-    @Test //@Test Esta é uma anotação que indica que este é um método de teste
-    fun insertPokemon() = runBlocking { // Aqui estamos definindo o nome do teste e indicando que ele pode ser executado
+    @Test //Aqui é o teste mesmo
+    fun insertPokemon() = runBlocking { //definindo o nome do teste e indicando que ele pode ser executado
 
-        val pokemon = PokemonEntity(1,"bulbasaur") // Aqui estamos criando uma instância de PokemonEntity
-        // com dados fictícios para simular um Pokémon
+        val pokemon = PokemonEntity(1,"bulbasaur") // instânciei PokemonEntity
 
-        // Agora, estamos configurando o comportamento do mock pokemonDao:
-        `when`(pokemonDao.getAllPokemons()).thenReturn(listOf(pokemon))// Quando o método getAllPokemons() for chamado,
-        // ele irá retornar uma lista contendo o Pokémon "bulbasaur"
+        //configurando o comportamento esperado do mock pokemonDao
+        `when`(pokemonDao.getAllPokemons()).thenReturn(listOf(pokemon))
+        // Quando o método getAllPokemons() for chamado, ele deveria retornar uma lista contendo o Pokémon "bulbasaur"
 
         // Em seguida, chamamos o método insertPokemon() do mock pokemonDao para inserir o Pokémon "bulbasaur" no banco de dados
         pokemonDao.insertPokemon(pokemon)
 
-        // Agora, recuperamos todos os Pokémon do banco de dados
+        // Agora, allPokemons recebe a lógica de buscar todos os Pokémon do banco de dados
         val allPokemons = pokemonDao.getAllPokemons()
 
         // Finalmente, estamos verificando se o Pokémon "bulbasaur" está presente na lista de todos os Pokémon recuperados do banco de dados
         assertTrue(allPokemons.contains(pokemon)) // Isso verifica se a lista de todos os Pokémon contém o Pokémon "bulbasaur"
     }
+
+    @Test
+    fun deletePokemon() = runBlocking {
+        val pokemon = PokemonEntity(1,"bulbasaur")
+
+        `when`(pokemonDao.getAllPokemons()).thenReturn(emptyList())
+
+        pokemonDao.insertPokemon(pokemon)
+
+        pokemonDao.deletePokemon(pokemon)
+
+        val allPokemons = pokemonDao.getAllPokemons()
+
+        assertFalse(allPokemons.contains(pokemon))
+    }
+
 }
