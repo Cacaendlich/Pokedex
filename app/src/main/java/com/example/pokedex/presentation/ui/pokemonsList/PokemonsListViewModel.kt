6280a9b9569c9e0ae2f89de641502fc809109256
Caca Endlich <camilaendlichpimenta.trab@gmail.com>
2024-05-17 14:55:43 -0300

    Update PokemonsListViewModel.kt

--- app/src/main/java/com/example/pokedex/presentation/ui/pokemonsList/PokemonsListViewModel.kt
+++ app/src/main/java/com/example/pokedex/presentation/ui/pokemonsList/PokemonsListViewModel.kt
@@ -106,11 +106,11 @@ class PokemonsListViewModel : ViewModel() {
         }.start()
     }
 
-    fun updateFavoriteState(favoriteList: List<PokemonEntity>, pokemon: Pokemon, position: Int, adapter: PokemonAdapter) {
-        val isFavorite = isFavorite(favoriteList, pokemon)
-        adapter.mPokemonList[position]?.favorite = !isFavorite
-        adapter.notifyItemChanged(position)
-    }
+//    fun updateFavoriteState(favoriteList: List<PokemonEntity>, pokemon: Pokemon, position: Int, adapter: PokemonAdapter) {
+//        val isFavorite = isFavorite(favoriteList, pokemon)
+//        adapter.mPokemonList[position]?.favorite = !isFavorite
+//        adapter.notifyItemChanged(position)
+//    }
 
     fun isFavorite(favoriteList: List<PokemonEntity>, pokemon: Pokemon): Boolean {
         return favoriteList.any {
