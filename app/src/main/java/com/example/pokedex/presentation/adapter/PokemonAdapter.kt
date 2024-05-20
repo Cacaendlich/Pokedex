081284d3858d1b9706fbfff9a13a9e06b5a451c6
Caca Endlich <camilaendlichpimenta.trab@gmail.com>
2024-05-20 14:44:45 -0300

    Update PokemonAdapter.kt

--- app/src/main/java/com/example/pokedex/presentation/adapter/PokemonAdapter.kt
+++ app/src/main/java/com/example/pokedex/presentation/adapter/PokemonAdapter.kt
@@ -65,7 +65,7 @@ class PokemonAdapter(
                 loadPokemonImage(pokemon.imageUrl)
                 mNameViewPokemon.text = currentItem.name
                 val isFavorite = isFavorite(favoriteList, pokemon)
-                Log.d("PokemonAdapter", "${pokemon.name} - Antes da atualização do estado de favorito:  ${pokemon.favorite}")
+                Log.d("PokemonAdapter", "${pokemon.name} - Valor atual de favorito:  ${pokemon.favorite}")
                 updateFavoriteIcon(isFavorite, mImageViewFavoriteOFF)
             }
         }
