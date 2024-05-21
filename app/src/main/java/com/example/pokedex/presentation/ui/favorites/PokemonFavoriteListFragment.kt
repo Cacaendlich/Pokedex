6d57fa85802e041b28c3fcf5c2171c55ede11b95
Caca Endlich <camilaendlichpimenta.trab@gmail.com>
2024-05-21 20:36:46 -0300

    Update PokemonFavoriteListFragment.kt

--- app/src/main/java/com/example/pokedex/presentation/ui/favorites/PokemonFavoriteListFragment.kt
+++ app/src/main/java/com/example/pokedex/presentation/ui/favorites/PokemonFavoriteListFragment.kt
@@ -1,9 +1,12 @@
 package com.example.pokedex.presentation.ui.favorites
 
+import android.content.res.Configuration
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.ProgressBar
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -11,10 +14,11 @@ import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.pokedex.data.local.model.PokemonEntity
 import com.example.pokedex.databinding.FragmentPokemonsListBinding
+import com.example.pokedex.domain.model.Pokemon
 import com.example.pokedex.presentation.adapter.PokemonAdapter
 import com.example.pokedex.presentation.ui.pokemonsList.PokemonsListViewModel
 
-class PokemonFavoriteListFragment : Fragment() {
+class PokemonFavoriteListFragment : Fragment(), PokemonAdapter.OnItemClickListener {
 
     private lateinit var binding: FragmentPokemonsListBinding
     private lateinit var favoriteListViewModel: PokemonFavoriteListViewModel
@@ -24,6 +28,8 @@ class PokemonFavoriteListFragment : Fragment() {
     private lateinit var mPokemonAdapter: PokemonAdapter
     private lateinit var progressBar: ProgressBar
     private lateinit var mfavoriteList: List<PokemonEntity>
+
+    private var currentPosition = 0
 
     companion object {
         fun newInstance() = PokemonFavoriteListFragment()
@@ -43,7 +49,73 @@ class PokemonFavoriteListFragment : Fragment() {
         favoriteListViewModel = ViewModelProvider(requireActivity())[PokemonFavoriteListViewModel::class.java]
         pokemonsListViewModel = ViewModelProvider(requireActivity())[PokemonsListViewModel::class.java]
 
+        mRecyclerView = binding.recyclerViewMain
+        mRecyclerView.setHasFixedSize(true)
 
+        pokemonsListViewModel.pokemonsState.observe(requireActivity()) { pokemons ->
+            pokemons?.let {
+                updateRecyclerView(pokemons)
+                progressBar.visibility = if (it.isEmpty()) View.VISIBLE else View.GONE
+            }
+        }
+
+        pokemonsListViewModel.isLoading.observe(requireActivity()) { isLoading ->
+            if (isLoading) {
+                binding.progressBarLoadMore.visibility = View.VISIBLE
+            } else {
+                binding.progressBarLoadMore.visibility = View.GONE
+            }
+        }
+
+        mRecyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+
+                val layoutManager = recyclerView.layoutManager as GridLayoutManager
+                val lastVisibleItemPosition = layoutManager.findLastCompletelyVisibleItemPosition()
+                val totalItemCount = layoutManager.itemCount
+                val limitLoading = lastVisibleItemPosition + 2
+
+                if (limitLoading >= totalItemCount && !pokemonsListViewModel.isLoading.value!!) {
+                    pokemonsListViewModel.loadMorePokemons()
+                }
+
+                currentPosition = lastVisibleItemPosition - 4
+
+            }
+        })
+
+        favoriteListViewModel.loadFavorites(requireContext()) { favorites ->
+            mfavoriteList = favorites
+            Log.d("PokemonsListFragment", "Está é a lista de favoritos: $mfavoriteList")
+        }
+
+    }
+
+    private fun updateRecyclerView(pokemons: List<Pokemon?>) {
+        pokemons.forEach { pokemon ->
+            pokemon?.let {
+                it.favorite = favoriteListViewModel.isFavorite(mfavoriteList, it)
+            }
+        }
+
+        mLayoutManager =
+            if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                GridLayoutManager(requireActivity(), 3)
+            } else {
+                GridLayoutManager(requireActivity(), 2)
+            }
+        mPokemonAdapter = PokemonAdapter(pokemons)
+
+        mRecyclerView.layoutManager = mLayoutManager
+        mRecyclerView.adapter = mPokemonAdapter
+
+        mPokemonAdapter.setOnItemClickListener(this)
+        mRecyclerView.scrollToPosition(currentPosition)
+    }
+
+    override fun onFavoriteClick(position: Int, imageView: ImageView) {
+        TODO("Not yet implemented")
     }
 
 }
