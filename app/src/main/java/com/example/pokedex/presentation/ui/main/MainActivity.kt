38923e1add2e83c7e1c9c53dd2b8d607e0519f96
Caca Endlich <camilaendlichpimenta.trab@gmail.com>
2024-05-23 09:55:26 -0300

    Update MainActivity.kt

--- app/src/main/java/com/example/pokedex/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/pokedex/presentation/ui/main/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.pokedex.R
 import com.example.pokedex.databinding.ActivityMainBinding
 import com.example.pokedex.presentation.ui.pokemonsList.PokemonsListFragment
+import com.example.pokedex.presentation.ui.favorites.PokemonFavoriteListFragment
 
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +16,12 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        binding.buttonFavorites.setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, PokemonFavoriteListFragment.newInstance())
+                .commit()
+        }
 
         supportFragmentManager.beginTransaction()
             .replace(R.id.fragment_container, PokemonsListFragment.newInstance())
