b9b73701fdee96f6ffb71d4ed6e8578c8cfbacb3
Caca Endlich <camilaendlichpimenta.trab@gmail.com>
2024-05-28 12:15:43 -0300

    Update LoginViewModelTest.kt

--- app/src/test/java/com/example/pokedex/presentation/ui/login/LoginViewModelTest.kt
+++ app/src/test/java/com/example/pokedex/presentation/ui/login/LoginViewModelTest.kt
@@ -3,60 +3,39 @@ package com.example.pokedex.presentation.ui.login
 import android.content.Context
 import android.content.SharedPreferences
 import org.junit.After
+import org.junit.Before
+import org.junit.Test
 import org.junit.jupiter.api.Assertions
 import org.mockito.ArgumentMatchers.anyInt
 import org.mockito.Mock
 import org.mockito.Mockito.anyString
-import org.mockito.Mockito.mock
 import org.mockito.Mockito.`when`
 import org.mockito.MockitoAnnotations
-import org.junit.Before
 
 class LoginViewModelTest {
 
-    private lateinit var loginViewModel: LoginViewModel //instância da classe LoginViewModel
+    private  var loginViewModel: LoginViewModel = LoginViewModel() // Instância da classe LoginViewModel
 
     @Mock
-    private lateinit var context: Context //Aqui, @Mock é uma anotação do Mockito
-    // que indica que a variável context será um objeto simulado (mock) da classe Context.
+    private lateinit var context: Context // Mock do Context para simular o ambiente Android
 
-
-    private lateinit var sharedPreferences: SharedPreferences //para simular um objeto SharedPreferences
+    @Mock
+    private lateinit var sharedPreferences: SharedPreferences // Mock do SharedPreferences para simular o armazenamento de dados
 
     @Before
     fun setUp() {
-        //Este método é executado antes de cada teste (@Test).
-        // Aqui, estamos configurando o ambient de teste.
+        // Método setUp(): Configuração do ambiente de teste
 
+        // Inicialização dos mocks
         MockitoAnnotations.openMocks(this)
 
-        context = mock(Context::class.java) //Criamos um objeto simulado (mock) da classe Context.
-        sharedPreferences = mockSharedPreferences()
-        `when`(context.getSharedPreferences(anyString(), anyInt())).thenReturn(sharedPreferences)
-        //Configuramos o comportamento do mock do Context.
-        // Estamos dizendo que quando o método getSharedPreferences() é chamado com qualquer String e qualquer Int,
-        // ele deve retornar o objeto simulado sharedPreferences.
-
-        loginViewModel = LoginViewModel() //Criamos uma instância da classe LoginViewModel.
     }
 
     @After
-    //Este método é executado após cada teste.
-    // Aqui, você pode limpar recursos ou redefinir o estado, se necessário.
     fun tearDown() {
+        // Método tearDown(): Limpeza de recursos após cada teste
     }
 
-
-    private fun mockSharedPreferences(): SharedPreferences {
-        // Esta é uma função auxiliar que cria um objeto simulado de SharedPreferences
-        // e configura seu comportamento.
-        val  sharedPreferences = mock(SharedPreferences::class.java)
-
-        `when`(sharedPreferences.getString(anyString(), anyString()))
-            .thenReturn("example@example.com")
-
-        return sharedPreferences
-    }
 
 //    @Test
 //    fun getLoginState() {
@@ -74,11 +53,15 @@ class LoginViewModelTest {
 //    fun getStoredLoginData() {
 //    }
 
-    @org.junit.Test
+    @Test
     fun sharedPrefsIsNotEmpty() {
-        // Define o comportamento esperado do objeto SharedPreferences simulado
-        `when`(sharedPreferences.getString(anyString(), anyString()))
-            .thenReturn("example@example.com")
+        // Teste para verificar se SharedPreferences não está vazio
+
+        // Configuração do mock do Context para retornar o mock de SharedPreferences
+        `when`(context.getSharedPreferences(anyString(), anyInt())).thenReturn(sharedPreferences)
+
+        // Configuração do mock de SharedPreferences para retornar um valor específico
+        `when`(sharedPreferences.getString(anyString(), anyString())).thenReturn("example@example.com")
 
         // Chama o método que será testado
         val result = loginViewModel.sharedPrefsIsNotEmpty(context)
