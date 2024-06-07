d5254cdce5381ca0733f00f814af9894cf9060bd
Caca Endlich <camilaendlichpimenta.trab@gmail.com>
2024-06-07 11:21:01 -0300

    testes a fazer

---
+++ app/src/test/java/com/example/pokedex/presentation/ui/favorites/PokemonFavoriteListViewModelTest.kt
@@ -0,0 +1,54 @@
+package com.example.pokedex.presentation.ui.favorites
+
+import org.junit.jupiter.api.Assertions.*
+
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class PokemonFavoriteListViewModelTest {
+
+    @BeforeEach
+    fun setUp() {
+    }
+
+    @AfterEach
+    fun tearDown() {
+    }
+
+    @Test
+    fun getPokemonsState() {
+    }
+
+    @Test
+    fun setPokemonsState() {
+    }
+
+    @Test
+    fun getFavoriteList() {
+    }
+
+    @Test
+    fun setFavoriteList() {
+    }
+
+    @Test
+    fun loadPokemons() {
+    }
+
+    @Test
+    fun loadFavorites() {
+    }
+
+    @Test
+    fun isFavorite() {
+    }
+
+    @Test
+    fun updateFavoritesList() {
+    }
+
+    @Test
+    fun removeFavorite() {
+    }
+}
