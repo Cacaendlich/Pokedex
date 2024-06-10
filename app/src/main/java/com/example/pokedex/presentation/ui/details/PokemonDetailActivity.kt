52c2949589fc678889841ba6b93398e24b742767
Caca Endlich <camilaendlichpimenta.trab@gmail.com>
2024-06-10 17:17:13 -0300

    back to activty main

--- app/src/main/java/com/example/pokedex/presentation/ui/details/PokemonDetailActivity.kt
+++ app/src/main/java/com/example/pokedex/presentation/ui/details/PokemonDetailActivity.kt
@@ -1,9 +1,13 @@
 package com.example.pokedex.presentation.ui.details
 
+import android.content.ContentValues.TAG
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.example.pokedex.databinding.ActivityPokemonDetailBinding
+import com.example.pokedex.presentation.ui.main.MainActivity
 
 class PokemonDetailActivity : AppCompatActivity() {
 
@@ -16,5 +20,30 @@ class PokemonDetailActivity : AppCompatActivity() {
         setContentView(view)
 
         pokemonDetailsViewModel = ViewModelProvider(this)[PokemonDetailsViewModel::class.java]
+
+        binding.imageViewBackToList.setOnClickListener {
+            val sourceFragment = intent.getStringExtra("sourceFragment")
+            Log.d(TAG, "Source Fragment: $sourceFragment")
+
+            when (sourceFragment) {
+                "PokemonsFavoriteListFragment" -> {
+                    Log.d(TAG, "Popping back to PokemonsFavoriteListFragment")
+                    supportFragmentManager.popBackStack("PokemonsFavoriteListFragment", 0)
+                }
+                "PokemonsListFragment" -> {
+                    Log.d(TAG, "Popping back to PokemonsListFragment")
+                    supportFragmentManager.popBackStack("PokemonsListFragment", 0)
+                }
+                else -> {
+                    Log.d(TAG, "Starting MainActivity")
+                    startActivity(Intent(this, MainActivity::class.java))
+                }
+            }
+        }
+
+
+    }
+    companion object {
+        private const val TAG = "PokemonDetailActivity"
     }
 }
