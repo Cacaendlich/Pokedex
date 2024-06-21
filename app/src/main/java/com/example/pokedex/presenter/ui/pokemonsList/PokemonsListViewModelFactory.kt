d2217b588d434f94fa95af0db545f02a5b0defd7
Caca Endlich <camilaendlichpimenta.trab@gmail.com>
2024-06-21 12:26:34 -0300

    Update PokemonsListViewModelFactory.kt

--- app/src/main/java/com/example/pokedex/presenter/ui/pokemonsList/PokemonsListViewModelFactory.kt
+++ app/src/main/java/com/example/pokedex/presenter/ui/pokemonsList/PokemonsListViewModelFactory.kt
@@ -3,9 +3,7 @@ package com.example.pokedex.presenter.ui.pokemonsList
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.pokedex.data.repository.PokemonRepository
-import com.example.pokedex.data.repository.PokemonRepositoryImpl
 
-//Fábrica para criar instâncias de PokemonsListViewModel
 class PokemonsListViewModelFactory(private val pokemonRepository: PokemonRepository) : ViewModelProvider.Factory{
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
