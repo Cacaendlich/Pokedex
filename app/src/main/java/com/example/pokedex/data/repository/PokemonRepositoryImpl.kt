77602cb3055a43104f954135d7178e4756025075
Caca Endlich <camilaendlichpimenta.trab@gmail.com>
2024-06-21 12:26:17 -0300

    Update PokemonRepositoryImpl.kt

--- app/src/main/java/com/example/pokedex/data/repository/PokemonRepositoryImpl.kt
+++ app/src/main/java/com/example/pokedex/data/repository/PokemonRepositoryImpl.kt
@@ -3,7 +3,6 @@ package com.example.pokedex.data.repository
 import com.example.pokedex.data.network.RetrofitClient
 import com.example.pokedex.domain.model.Pokemon
 
-//Implementa PokemonRepository
 class PokemonRepositoryImpl(private val retrofitClient: RetrofitClient) : PokemonRepository {
     override suspend fun listPokemons(limit: Int, offset: Int): List<Pokemon?> {
         val pokemonsApiResult = retrofitClient.listPokemons(limit, offset)
