222aa8be4bb5cdf1623fa348edad081d0447f9ae
Caca Endlich <camilaendlichpimenta.trab@gmail.com>
2024-06-24 16:32:11 -0300

    Create RecyClerViewUtil.kt

---
+++ app/src/main/java/com/example/pokedex/presenter/ui/pokemonsList/RecyClerViewUtil.kt
@@ -0,0 +1,50 @@
+package com.example.pokedex.presenter.ui.pokemonsList
+
+import android.content.res.Configuration
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.pokedex.data.local.model.PokemonEntity
+import com.example.pokedex.domain.model.Pokemon
+import com.example.pokedex.presenter.adapter.PokemonAdapter
+import com.example.pokedex.presenter.ui.favorites.PokemonFavoriteListViewModel
+
+class RecyclerViewUtil(
+    private val recyclerView: RecyclerView,
+    private val favoriteListViewModel: PokemonFavoriteListViewModel,
+    private val mfavoriteList: List<PokemonEntity>,
+    private val fragment: Fragment
+) {
+    private lateinit var pokemonAdapter: PokemonAdapter
+    private lateinit var layoutManager: GridLayoutManager
+
+    fun setupAdapter(pokemons: List<Pokemon?>, onItemClickListener: PokemonAdapter.OnItemClickListener) {
+        pokemonAdapter = PokemonAdapter(pokemons)
+        recyclerView.adapter = pokemonAdapter
+        pokemonAdapter.setOnItemClickListener(onItemClickListener)
+    }
+
+    fun setupLayoutManager() {
+        layoutManager = if (fragment.resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            GridLayoutManager(fragment.requireActivity(), 3)
+        } else {
+            GridLayoutManager(fragment.requireActivity(), 2)
+        }
+        recyclerView.layoutManager = layoutManager
+    }
+
+    fun updatePokemonFavorites(pokemons: List<Pokemon?>) {
+        pokemons.forEach { pokemon ->
+            pokemon?.let {
+                it.favorite = favoriteListViewModel.isFavorite(mfavoriteList, it)
+            }
+        }
+    }
+
+    fun scrollToPosition(currentPosition: Int) {
+        recyclerView.scrollToPosition(currentPosition)
+    }
+
+    fun getAdapter(): PokemonAdapter = pokemonAdapter
+    fun getLayoutManager(): GridLayoutManager = layoutManager
+}
