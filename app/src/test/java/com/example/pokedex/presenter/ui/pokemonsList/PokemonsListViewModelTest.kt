365b1c4ab6e22ee5bd300c11b8aa9e3ab277508f
Caca Endlich <camilaendlichpimenta.trab@gmail.com>
2024-06-26 11:54:20 -0300

    Update PokemonsListViewModelTest.kt

--- app/src/test/java/com/example/pokedex/presenter/ui/pokemonsList/PokemonsListViewModelTest.kt
+++ app/src/test/java/com/example/pokedex/presenter/ui/pokemonsList/PokemonsListViewModelTest.kt
@@ -1,52 +1,76 @@
 package com.example.pokedex.presenter.ui.pokemonsList
 
+import com.example.pokedex.data.repository.PokemonRepository
+import com.example.pokedex.domain.model.Pokemon
+import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.MockitoAnnotations
 
 class PokemonsListViewModelTest {
 
+    private lateinit var viewModel: PokemonsListViewModel
+
+    @Mock
+    private lateinit var pokemonRepository: PokemonRepository
+
     @BeforeEach
     fun setUp() {
+        MockitoAnnotations.openMocks(this)
+        viewModel = PokemonsListViewModel(pokemonRepository)
     }
 
     @AfterEach
     fun tearDown() {
     }
 
-    @Test
-    fun getPokemonsState() {
-    }
+//    @Test
+//    fun getPokemonsState() {
+//    }
+//
+//    @Test
+//    fun setPokemonsState() {
+//    }
+//
+//    @Test
+//    fun isLoading() {
+//    }
+//
+//    @Test
+//    fun setLoading() {
+//    }
 
     @Test
-    fun setPokemonsState() {
-    }
+    fun loadPokemons() = runBlocking {
+        val limit = 14
+        val offset = 0
 
-    @Test
-    fun isLoading() {
-    }
+        val listMock = listOf(
+            Pokemon(1, "bulbasaur", true),
+            Pokemon(2, "ivysaur", false),
+            Pokemon(3, "venosaur", false)
+        )
 
-    @Test
-    fun setLoading() {
-    }
+        `when`(pokemonRepository.listPokemons(limit, offset)).thenReturn(listMock)
 
-    @Test
-    fun loadInitialPokemons() {
-    }
+        val result = viewModel.loadPokemons(limit, offset)
 
-    @Test
-    fun loadPokemons() {
+        Assertions.assertEquals(listMock, result)
     }
-
-    @Test
-    fun loadMorePokemons() {
-    }
-
-    @Test
-    fun refreshPokemons() {
-    }
-
-    @Test
-    fun testSetLoading() {
-    }
+//
+//    @Test
+//    fun loadMorePokemons() {
+//    }
+//
+//    @Test
+//    fun refreshPokemons() {
+//    }
+//
+//    @Test
+//    fun testSetLoading() {
+//    }
 }
