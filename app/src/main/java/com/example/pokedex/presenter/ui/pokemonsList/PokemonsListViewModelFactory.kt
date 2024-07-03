b27ec276b732b695673ef21f0ac85a2fb7d7b7c6
Caca Endlich <camilaendlichpimenta.trab@gmail.com>
2024-07-03 11:12:51 -0300

    Revert "Update PokemonsListViewModelFactory.kt"
    This reverts commit 90ede482671dc4e3b15492e9d60171bd7f6b3e63.

--- app/src/main/java/com/example/pokedex/presenter/ui/pokemonsList/PokemonsListViewModelFactory.kt
+++ app/src/main/java/com/example/pokedex/presenter/ui/pokemonsList/PokemonsListViewModelFactory.kt
@@ -3,22 +3,15 @@ package com.example.pokedex.presenter.ui.pokemonsList
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.pokedex.data.repository.PokemonRepository
-import com.example.pokedex.presenter.ui.favorites.PokemonFavoriteListViewModel
 import com.example.pokedex.presenter.ui.pokemonsList.useCase.LoadPokemonsUseCase
 
 class PokemonsListViewModelFactory(private val pokemonRepository: PokemonRepository) : ViewModelProvider.Factory{
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        val loadPokemonsUseCase = LoadPokemonsUseCase(pokemonRepository)
-        return when {
-            modelClass.isAssignableFrom(PokemonsListViewModel::class.java) -> {
-                PokemonsListViewModel(loadPokemonsUseCase) as T
-            }
-            modelClass.isAssignableFrom(PokemonFavoriteListViewModel::class.java) -> {
-                PokemonFavoriteListViewModel(loadPokemonsUseCase) as T
-            }
-
-            else -> throw IllegalArgumentException("Unknown ViewModel class")
+        if (modelClass.isAssignableFrom(PokemonsListViewModel::class.java)) {
+            val loadPokemonsUseCase = LoadPokemonsUseCase(pokemonRepository)
+            return PokemonsListViewModel(loadPokemonsUseCase) as T
         }
+        throw IllegalArgumentException("Unknown ViewModel class")
     }
 }
