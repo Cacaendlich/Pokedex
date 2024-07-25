f9f3cd1e0139a21de58f19e7d5236c26aa0559fd
Caca Endlich <camilaendlichpimenta.trab@gmail.com>
2024-07-25 12:20:10 -0300

    Update PokemonFavoriteListViewModelTest.kt

--- app/src/test/java/com/example/pokedex/presenter/ui/favorites/PokemonFavoriteListViewModelTest.kt
+++ app/src/test/java/com/example/pokedex/presenter/ui/favorites/PokemonFavoriteListViewModelTest.kt
@@ -2,39 +2,17 @@ package com.example.pokedex.presenter.ui.favorites
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.Observer
-import com.example.pokedex.data.local.model.PokemonEntity
 import com.example.pokedex.data.repository.api.PokemonApiRepository
 import com.example.pokedex.data.repository.local.PokemonLocalRepository
 import com.example.pokedex.domain.model.Pokemon
-import kotlinx.coroutines.test.runTest
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.junit.jupiter.api.Assertions
 import org.mockito.Mock
-import org.mockito.Mockito
-import org.mockito.Mockito.`when`
 import org.mockito.MockitoAnnotations
 
 class PokemonFavoriteListViewModelTest {
-    private val pokemonListMock = listOf(
-        Pokemon(1, "bulbasaur"),
-        Pokemon(2, "ivysaur"),
-        Pokemon(3, "venusaur"),
-        Pokemon(4, "charmander")
-    )
-    private val pokemonFavoriteListMock = listOf(
-        PokemonEntity(1, "bulbasaur"),
-        PokemonEntity(2, "ivysaur")
-    )
-
-    private val pokemonMock = Pokemon(4, "charmander")
-    private val pokemonMock2 = Pokemon(1, "bulbasaur")
-    private val pokemonFavoriteMock = PokemonEntity(1, "Bulbasaur")
-
-
-
 
     @JvmField
     @Rule
@@ -54,7 +32,6 @@ class PokemonFavoriteListViewModelTest {
         MockitoAnnotations.openMocks(this)
         viewModel = PokemonFavoriteListViewModel(pokemonRepository, pokemonLocalRepository)
         viewModel.pokemonsState.observeForever(observer)
-        `when`(viewModel.isFavorite(pokemonFavoriteListMock, pokemonMock2)).thenReturn(true)
 
     }
 
@@ -63,52 +40,9 @@ class PokemonFavoriteListViewModelTest {
         viewModel.pokemonsState.removeObserver(observer)
     }
 
-//    @Test
-//    fun loadPokemons() {
-//    }
-//
-//    @Test
-//    fun loadFavorites() {
-//    }
-
-    //updateFavoritesList()
     @Test
-    fun `updateFavoritesList pokemon is favorite, delete Favorite successfully`() = runTest{
-        val pokemon = pokemonMock2
-        val favoriteList = pokemonFavoriteListMock
+    fun removeFavorite(){
 
-
-        viewModel.updateFavoritesList(pokemon, favoriteList)
-
-        Mockito.verify(pokemonLocalRepository).deleteFavorite(pokemon.number)
     }
 
-    @Test
-    fun  `test checking if Pokemon is favorite when it is in the list`() {
-        val favoritePokemon = Pokemon(1, "bulbasaur")
-
-        val resul = viewModel.isFavorite(pokemonFavoriteListMock, favoritePokemon)
-
-        Assertions.assertTrue(resul)
-    }
-
-    @Test
-    fun `test checking if Pokemon is favorite when it is not in the list`() {
-        val favoritePokemon = Pokemon(3, "venosaur")
-
-        val resul = viewModel.isFavorite(pokemonFavoriteListMock, favoritePokemon)
-
-        Assertions.assertFalse(resul)
-    }
-
-//    @OptIn(ExperimentalCoroutinesApi::class)
-//    @Test
-//    fun removeFavorite() = runTest{
-//
-//        viewModel.removeFavorite(pokemonMock)
-//
-//        advanceUntilIdle()
-//
-//        Mockito.verify(pokemonLocalRepository).deleteFavorite(pokemonMock.number)
-//    }
 }
