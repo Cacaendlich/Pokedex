557a9cfe434edd2dc2b7e699b6b517ddc4b862ad
Caca Endlich <camilaendlichpimenta.trab@gmail.com>
2024-08-20 11:33:25 -0300

    Update PokemonFavoriteListViewModel.kt

--- app/src/main/java/com/example/pokedex/presenter/ui/favorites/PokemonFavoriteListViewModel.kt
+++ app/src/main/java/com/example/pokedex/presenter/ui/favorites/PokemonFavoriteListViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.pokedex.presenter.ui.favorites
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
