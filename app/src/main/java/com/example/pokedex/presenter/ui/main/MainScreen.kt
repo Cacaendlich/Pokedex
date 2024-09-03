0d00afb52cb3e12e3cf9c9ea28ad78959e4a6ed0
Caca Endlich <camilaendlichpimenta.trab@gmail.com>
2024-09-03 14:25:51 -0300

    mainScreen e PokemonListScreen
    layout do card check
    header e floatButton check

---
+++ app/src/main/java/com/example/pokedex/presenter/ui/main/MainScreen.kt
@@ -0,0 +1,119 @@
+package com.example.pokedex.presenter.ui.main
+
+import android.content.Context
+import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.TileMode
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.pokedex.R
+import com.example.pokedex.presenter.ui.main.ui.theme.PokedexTheme
+import com.example.pokedex.presenter.ui.theme.Black
+import com.example.pokedex.presenter.ui.theme.Red
+import com.example.pokedex.presenter.ui.theme.White
+import com.example.pokedex.presenter.ui.theme.Yellow
+
+
+@Composable
+fun MainScreen(onClick: () -> Unit) {
+    val context = LocalContext.current
+    Column(
+        verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                Brush.verticalGradient(
+                    listOf(Black, Red),
+                    startY = 1000.0f,
+                    endY = Float.POSITIVE_INFINITY,
+                    tileMode = TileMode.Clamp
+                )
+            )
+    ) {
+        HeadingPokedex(context = context)
+        Row {
+            FavoriteActionButton(
+                onClick = onClick,
+                modifier = Modifier
+                    .padding(vertical = 20.dp)
+            )
+        }
+    }
+}
+
+@Composable
+fun HeadingPokedex(context: Context){
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 4.dp, horizontal = 12.dp)
+    ) {
+        Text(
+            text = "Pokedex",
+            color = White,
+            fontWeight = FontWeight.Bold,
+            fontSize = 22.sp
+        )
+
+        Image(
+            modifier = Modifier
+                .size(50.dp)
+                .clickable {
+                    Log.e("ClickableImage", "Imagem clicada!")
+                }
+            ,
+            painter = painterResource(id = R.drawable.battle_icon),
+            contentDescription = "Ícone de seta vermelha apontando para a esquerda, utilizado para retornar à tela anterior."
+        )
+    }
+}
+
+@Composable
+fun FavoriteActionButton(onClick: () -> Unit, modifier: Modifier){
+    FloatingActionButton(
+        onClick = { onClick() },
+        modifier = modifier,
+        containerColor = Yellow,
+        contentColor = Red,
+        shape = CircleShape
+    ) {
+        Icon(
+            imageVector =  Icons.Filled.Favorite,
+            contentDescription = "Black heart icon that navigates to the favorites list",
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview3() {
+    PokedexTheme {
+        MainScreen(onClick = { Log.e("FavoriteActionButton", "clicado!") })
+    }
+}
---
+++ app/src/main/java/com/example/pokedex/presenter/ui/pokemonsList/PokemonListScreen.kt
@@ -0,0 +1,138 @@
+package com.example.pokedex.presenter.ui.pokemonsList
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
+import com.example.pokedex.R
+import com.example.pokedex.data.model.PokemonType
+import com.example.pokedex.data.model.Stat
+import com.example.pokedex.data.model.Stats
+import com.example.pokedex.data.model.Type
+import com.example.pokedex.domain.model.Pokemon
+import com.example.pokedex.presenter.ui.pokemonsList.ui.theme.PokedexTheme
+import com.example.pokedex.presenter.ui.theme.White
+
+
+@Composable
+fun PokemonList(pokemons: List<Pokemon>) {
+
+   LazyVerticalGrid(
+       columns = GridCells.Fixed(2),
+       modifier = Modifier
+           .fillMaxSize()
+   ) {
+       items(pokemons.size){ index->
+           PokemonItem(pokemon = pokemons[index], color = -7815000)
+
+       }
+   }
+}
+
+@OptIn(ExperimentalGlideComposeApi::class)
+@Composable
+fun PokemonItem(pokemon: Pokemon, color: Int){
+    val pokemonColor = Color(color)
+    Card(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(20.dp),
+        colors = CardDefaults.cardColors(pokemonColor)
+    ){
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(5.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.favorite_off),
+                contentDescription = "heart whith empty, favorite off",
+                modifier = Modifier
+                    .align(Alignment.End)
+                    .size(50.dp)
+                    .padding(horizontal = 6.dp)
+            )
+            GlideImage(
+                model = pokemon.imageUrl,
+                contentDescription = "",
+            )
+
+            Text(
+                text = pokemon.name,
+                color = White,
+                fontWeight = FontWeight.Bold,
+                fontSize = 22.sp
+            )
+        }
+    }
+}
+
+val pokemon1 = Pokemon(
+    number = 1,
+    name = "Bulbasaur",
+    height = 7,
+    weight = 69,
+    stats = listOf(
+        Stats(base_stat = 45, stat = Stat(name = "hp")),
+        Stats(base_stat = 49, stat = Stat(name = "attack")),
+        Stats(base_stat = 49, stat = Stat(name = "defense")),
+        Stats(base_stat = 65, stat = Stat(name = "special-attack")),
+        Stats(base_stat = 65, stat = Stat(name = "special-defense")),
+        Stats(base_stat = 45, stat = Stat(name = "speed"))
+    ),
+    type = listOf(
+        PokemonType(slot = 1, type = Type(name = "grass")),
+        PokemonType(slot = 2, type = Type(name = "poison"))
+    ),
+    favorite = false
+)
+val pokemon2 = Pokemon(
+    number = 1,
+    name = "Bulbasaur",
+    height = 7,
+    weight = 69,
+    stats = listOf(
+        Stats(base_stat = 45, stat = Stat(name = "hp")),
+        Stats(base_stat = 49, stat = Stat(name = "attack")),
+        Stats(base_stat = 49, stat = Stat(name = "defense")),
+        Stats(base_stat = 65, stat = Stat(name = "special-attack")),
+        Stats(base_stat = 65, stat = Stat(name = "special-defense")),
+        Stats(base_stat = 45, stat = Stat(name = "speed"))
+    ),
+    type = listOf(
+        PokemonType(slot = 1, type = Type(name = "grass")),
+        PokemonType(slot = 2, type = Type(name = "poison"))
+    ),
+    favorite = false
+)
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview4() {
+    PokedexTheme {
+        PokemonList(pokemons = listOf(pokemon1, pokemon2, pokemon1, pokemon2, pokemon1, pokemon2, pokemon1, pokemon2, pokemon1, pokemon2))
+    }
+}
